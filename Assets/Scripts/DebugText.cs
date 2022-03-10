1f3780fd1dfc88fef485303363c221e6ae0021d8
Ville <kuvaja.ville@gmail.com>
2022-03-11 00:30:33 +0200

    fixed a layer that was missing

---
+++ Assets/Scripts/DebugText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugText : MonoBehaviour
+{
+
+    public static DebugText instance;
+
+    private Text text;
+
+    void Awake() 
+    {
+
+        instance=this;
+        text=GetComponent<Text>();
+
+    }
+
+    public void SetText(string str) {
+
+        text.text=str;
+
+    }
+
+    public void AppendText(string str) {
+
+        text.text+=str;
+
+    }
+
+}
--- Assets/Scripts/UserInput.cs
+++ Assets/Scripts/UserInput.cs
@@ -10,7 +10,7 @@ public class UserInput : MonoBehaviour
 
     public bool allowInput=false;
 
-    private float dragThreshold=5f;
+    private float dragThreshold=10f;
 
     private bool hasInput=false;
     private Vector3 firstTouchPos;
@@ -82,12 +82,13 @@ public class UserInput : MonoBehaviour
         hasInput=false;
 
         Vector3 drag=touchPos-firstTouchPos;
+
         if(drag.magnitude<dragThreshold) {
 
             Ray ray=Camera.main.ScreenPointToRay(touchPos);
             RaycastHit hit;
             if(Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, floorLayerMask)) {
-                
+
                 if(Character.instance) {
                     
                     Vector3 destination=hit.point;
@@ -115,86 +116,3 @@ public class UserInput : MonoBehaviour
     }
 
 }
-
-/*
-//---------------------------------
-// VARIABLES
-
-    public Character character;
-    public CameraRig cameraRig;
-
-    private float dragThreshold=5f;
-
-    private Vector3 firstTouchPos;
-
-//---------------------------------
-// EVENTS
-
-    void Start() 
-    {
-
-    }
-
-//------------
-
-    void Update()
-    {
-
-        #if UNITY_EDITOR || UNITY_STANDALONE_WIN
-
-            if(Input.GetMouseButtonDown(0)) BeginTouch(Input.mousePosition);
-            if(Input.GetMouseButtonUp(0)) EndTouch(Input.mousePosition);
-
-        #elif UNITY_IOS || UNITY_ANDROID
-
-            if(Input.touches.Length>0 && Input.touches[0].phase==TouchPhase.Began) BeginTouch(Input.touches[0].position);
-            if(Input.touches.Length==0 || Input.touches[0].phase==TouchPhase.Ended) EndTouch(Input.touches[0].position);
-
-        #endif
-
-    }
-
-    private void BeginTouch(Vector3 touchPos) {
-
-        firstTouchPos=touchPos;
-
-    }
-
-    private void EndTouch(Vector3 touchPos) {
-
-        Vector3 drag=touchPos-firstTouchPos;
-        if(drag.magnitude<dragThreshold) {
-
-            MoveCharacter(touchPos);
-
-        } else {
-
-            Debug.Log("dragged");
-
-        }
-
-    }
-
-    private void MoveCharacter(Vector3 touchPos) {
-
-        Vector3 screenPos=Camera.main.WorldToScreenPoint(character.transform.position);
-        screenPos.z=0f;
-
-        Vector3 dir=touchPos-screenPos;
-
-        Vector3 worldDir=dir.normalized;
-        worldDir=new Vector3(worldDir.x, 0f, worldDir.y);
-        worldDir=cameraRig.transform.TransformPoint(worldDir)-cameraRig.transform.position;
-
-        float walkDistance=Mathf.Sqrt(Mathf.Pow(dir.x, 2f)+Mathf.Pow(dir.y, 2f));
-        walkDistance/=30f;
-        if(dir.y>0f) walkDistance*=2f;
-
-        Vector3 destination=character.transform.position;
-        destination+=worldDir*walkDistance;
-        character.WalkTo(destination);
-
-    }
-
-}
-*/
